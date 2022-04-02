c4eea60a3d73f04407096dbb0da04ba7e7e30c1b
premp74 <84513572+premp74@users.noreply.github.com>
2022-04-01 23:13:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-from picamera import PiCamera
-from time import sleep
-import RPi.GPIO as GPIO
-import time
+import face_recognition
+import cv2
+import numpy as np
+import os
+import glob
 
-pin = 21
+faces_encodings = []
+faces_names = []
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(pin, GPIO.OUT)
+cur_direc = os.getcsd()
 
+path = os.path.join(cur_direc, 'data/faces/')
 
+list_of_files = [f for f in glob.glob(path+'*.jpg')]
 
+number_files = len(list_of_files)
 
+names = list_of_files.copy()
 
-camera = PiCamera()
-GPIO.output(pin, GPIO.HIGH)
-camera.start_preview()
+for i in range(number_files):
+    globals()['image_{}'.format(i)] = face_recognition.load_image_file(list_of_files[i])
+    globals()['image_encoding_{}'.format(i)] = face_recognition.face_encoding(globals()['image_{}'.format(i)])[0]
+    faces_encoding.append(globals()['image_encoding_()'.format(i)])
+    
+    names[i] = names[i].replace(cur_direc, "")
+    faces_names.append(names[i])
 
-sleep(100)
-camera.stop_preview()
-
-
-
-  
+                
